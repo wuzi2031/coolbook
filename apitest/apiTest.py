a34ddd805528a43509bb7a2bbb2520dfa15b055e
zhimingwu <wuzhiming_cd@keruyun.com>
2018-04-27 16:59:32 +0800

    格式调整

--- apitest/apiTest.py
+++ apitest/apiTest.py
@@ -1,15 +1,22 @@
-import unittest,logging
+import unittest, logging
 from apitest import httpRequests
+
 
 class UserOptionTest(unittest.TestCase):
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
+
     def test_login(self):
-        header={'dd':'dd'}
-        hUtil=httpRequests.HttpUtil(url='https://www.baidu.com',method=httpRequests.Method.POST,header=header)
-        re=hUtil.request()
+        header = {'dd': 'dd'}
+        hUtil = httpRequests.HttpUtil(url='https://www.baidu.com',
+                                      method=httpRequests.Method.POST,
+                                      header=header)
+        re = hUtil.request()
         print(re)
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     unittest.main(verbosity=2)
--- apitest/httpRequests.py
+++ apitest/httpRequests.py
@@ -1,29 +1,31 @@
 import requests
 from enum import Enum
 
+
 class Method(Enum):
-    POST='post'
-    GET='get'
-    DELETE='delete'
-    PUT='put'
+    POST = 'post'
+    GET = 'get'
+    DELETE = 'delete'
+    PUT = 'put'
+
 
 class HttpUtil:
-    def __init__(self,url="", data="", header="", method=Method.GET):
-        self.url=url
-        self.data=data
+    def __init__(self, url="", data="", header="", method=Method.GET):
+        self.url = url
+        self.data = data
         self.__initHeader(header)
-        self.method=method
+        self.method = method
 
-    def __initHeader(self,header):
+    def __initHeader(self, header):
         self.headers = {'Host': self.url[8:],
-                   'Connection': 'keep-alive',
-                   'Cache-Control': 'max-age=0',
-                   'Accept': 'text/html, */*; q=0.01',
-                   'Accept-Encoding': 'gzip,deflate,br',
-                   'Accept-Language': 'zh-CN,zh;q=0.8,ja;q=0.6'
-                   }
+                        'Connection': 'keep-alive',
+                        'Cache-Control': 'max-age=0',
+                        'Accept': 'text/html, */*; q=0.01',
+                        'Accept-Encoding': 'gzip,deflate,br',
+                        'Accept-Language': 'zh-CN,zh;q=0.8,ja;q=0.6'
+                        }
         self.headers.update(header)
-        print("header:"+self.headers.__str__())
+        print("header:" + self.headers.__str__())
 
     def request(self):
         re = object
@@ -31,8 +33,8 @@ class HttpUtil:
         if self.method == Method.POST:
             try:
                 re = requests.post(self.url, headers=self.headers, data=self.data)
-                print("re:"+re.__str__())
-                re= re.json()
+                print("re:" + re.__str__())
+                re = re.json()
             except Exception as e:
                 re = e
                 isexcept = True
